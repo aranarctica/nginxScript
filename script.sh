53621fb3bf4a31a90308dec8c7adab32fad00c81
DW31 <ik012982i9@H012982.NET>
2018-02-07 12:24:32 +0100

    primer script

---
+++ script.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+apt-get update
+apt-get upgrade
+sudo apt-get install nginx -y
+sed 's/servername localhost/servername nirelocal/g' /etc/nginx/conf/nginx.conf > /etc/nginx/conf/nginx2.conf
+sed 's/listen 80 /listen 8080/g' /etc/nginx/conf/nginx.conf > /etc/nginx/conf/nginx2.conf
+apt-get restart nginx
+apt-get update
+sudo apt-get install openssl -y
+openssl genrsa -des3 -out example.com.key 1024
+openssl rsa -in example.com.key -out example.com.key.unsecure
+openssl req -new -key example.com.key -out example.com.csr
+
+
+
